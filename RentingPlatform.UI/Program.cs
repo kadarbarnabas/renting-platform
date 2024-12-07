0f19e2389ff7bdcaba29effda37498b54fa0a1a3
kadarbarnabas <113166617+kadarbarnabas@users.noreply.github.com>
2024-12-07 13:02:34 +0100

    Update the whole UI

--- RentingPlatform.UI/Program.cs
+++ RentingPlatform.UI/Program.cs
@@ -2,6 +2,9 @@ using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using RentingPlatform.UI;
 using RentingPlatform.UI.Services;
+using Blazorise;
+using Blazorise.Bootstrap5;
+using Blazorise.Icons.FontAwesome;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
@@ -10,5 +13,12 @@ builder.RootComponents.Add<HeadOutlet>("head::after");
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri("http://localhost:8080") });
 builder.Services.AddScoped<IUserService, UserService>();
 
+builder.Services
+    .AddBlazorise( options =>
+    {
+        options.Immediate = true;
+    } )
+    .AddBootstrap5Providers()
+    .AddFontAwesomeIcons();
 
 await builder.Build().RunAsync();
